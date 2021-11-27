d6bef82e9db0a6403b514dea5154c38c5e49738c
appvinio <filigaw048@student.polsl.pl>
2021-11-27 21:32:29 +0100

    * HospitalDbContext.cs: add missing files for previous commit:   3c18a23df5c6d5868bebe30874256253a388fef6
    * Role.cs:
    * User.cs:
    * IHashPassword.cs:
    * MedicalRecord.cs:
    * CreateDoctorDto.cs:
    * DoctorService.cs:
    * CreateDoctorDtoValidator.cs:
    * 20211127191216_initmigration.cs:
    * HospitalDbContextModelSnapshot.cs:
    * 20211127191216_initmigration.Designer.cs:

---
+++ hospital/Services/DoctorService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AutoMapper;
+using hospital.Entities;
+using hospital.Models;
+
+namespace hospital.Services
+{
+    public interface IDoctorService {
+        int Create(CreateDoctorDto dto);
+    }
+
+    public class DoctorService : IDoctorService
+    {
+        private readonly HospitalDbContext _dbContext;
+        private readonly IMapper _mapper;
+        public DoctorService(HospitalDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public int Create(CreateDoctorDto dto)
+        {
+            var user = _mapper.Map<User>(dto);
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+            return user.Id;
+        }
+    }
+}
---
+++ hospital/Validators/CreateDoctorDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+using hospital.Entities;
+using hospital.Models;
+
+namespace hospital.Validators
+{
+    public class CreateDoctorDtoValidator : AbstractValidator<CreateDoctorDto>
+    {
+        public CreateDoctorDtoValidator(HospitalDbContext dbContext)
+        {
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.Any(u => u.Email == value);
+                    if (emailInUse)
+                    {
+                        context.AddFailure("Email", "That email is taken");
+                    }
+                });
+        }
+    }
+}
