264d0cba669c5d088dbfb2227261d0b02e284359
appvinio <filigaw048@student.polsl.pl>
2021-12-14 09:26:48 +0100

    * UnitTest1.cs: * Hospital.Tests.csproj:
    * hospital.sln: add test

---
+++ Hospital.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using hospital.Helpers;
+using Xunit;
+
+namespace Hospital.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void RandomString_ForLength7_ReturnsRandomStringWithLength7()
+        {
+            // arange
+            int length = 7;
+
+            // act
+            string result = RandomStringGenerator.RandomString(length);
+
+            // assert
+            Assert.Equal(length, result.Length);
+        }
+    }
+}
